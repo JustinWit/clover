b90774044d73a48b36650be59c3f6d1996d6c042
Justin Wit <justinwit00@gmail.com>
2024-04-25 12:10:20 -0400

    Code clean up

--- clover/marl/waypoints.py
+++ clover/marl/waypoints.py
@@ -57,8 +57,7 @@ class Drone():
             self.speed = dist / ((t - prev_t) / 1000)
 
             # lookahead distances
-            c_dist = dist / 4  # fix the distance we are going to navigate to
-            proj_dist = c_dist * 2  # fix how far we are projecting that point
+            proj_dist = dist / 2  # fix how far we are projecting that point
             
             # loop til in tolerance of goal updating lookahead point as we go
             while not rospy.is_shutdown():
@@ -70,17 +69,8 @@ class Drone():
                     break
                 # update proj_x and proj_y
                 else:
-                    # dist_num = abs((abs_x - prev_x) * (telem.y - prev_y) - (telem.x - prev_x) * (abs_y - prev_y))  # from wikipedia distance from point to line
-                    # p_error = dist_num / dist  # orthogonal distance from drone to real path
-                    # dist_p = math.sqrt((telem.x - prev_x)**2 + (telem.y - prev_y)**2)  # drone distance from start point
-
-
-                    # dist_line = math.sqrt(abs(dist_p**2 - p_error**2)) + math.sqrt(abs(c_dist**2 - p_error**2))
-
-                    linept = abs_x, abs_y
-
-                    proj_x = telem.x + ((dist + proj_dist) / dist) * (linept[0] - telem.x)
-                    proj_y = telem.y + ((dist + proj_dist) / dist) * (linept[1] - telem.y)                
+                    proj_x = telem.x + ((dist + proj_dist) / dist) * (abs_x - telem.x)
+                    proj_y = telem.y + ((dist + proj_dist) / dist) * (abs_y - telem.y)                
 
                 # call clover navigate with out projection points
                 navigate(x=proj_x, y=proj_y, z=abs_z, speed=self.speed, frame_id=self.frame_id)
